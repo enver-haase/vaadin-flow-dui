5d2f382d7b45fed861f68fa2521360b24f99b995
Patrick Schmidt <patrick@codecamp.de>
2021-09-17 14:37:56 +0200

    Add constants for CSS properties.

---
+++ src/main/java/de/codecamp/vaadin/flowdui/util/CssProperties.java
@@ -0,0 +1,665 @@
+package de.codecamp.vaadin.flowdui.util;
+
+/**
+ * Constants for the more commonly useful groups of CSS properties.
+ *
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">MDN Web Docs: CSS</a>
+ */
+public interface CssProperties
+{
+
+  /*
+   * Background Properties
+   */
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/background">MDN Web Docs</a>
+   */
+  String background = "background";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/background-attachment">MDN Web
+   *      Docs</a>
+   */
+  String backgroundAttachment = "background-attachment";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/background-clip">MDN Web
+   *      Docs</a>
+   */
+  String backgroundClip = "background-clip";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/background-color">MDN Web
+   *      Docs</a>
+   */
+  String backgroundColor = "background-color";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/background-image">MDN Web
+   *      Docs</a>
+   */
+  String backgroundImage = "background-image";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/background-origin">MDN Web
+   *      Docs</a>
+   */
+  String backgroundOrigin = "background-origin";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/background-position">MDN Web
+   *      Docs</a>
+   */
+  String backgroundPosition = "background-position";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/background-repeat">MDN Web
+   *      Docs</a>
+   */
+  String backgroundRepeat = "background-repeat";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/background-size">MDN Web
+   *      Docs</a>
+   */
+  String backgroundSize = "background-size";
+
+
+  /*
+   * Border Properties
+   */
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border">MDN Web Docs</a>
+   */
+  String border = "border";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-bottom">MDN Web Docs</a>
+   */
+  String borderBottom = "border-bottom";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-bottom-color">MDN Web
+   *      Docs</a>
+   */
+  String borderBottomColor = "border-bottom-color";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-bottom-left-radius">MDN
+   *      Web Docs</a>
+   */
+  String borderBottomLeftRadius = "border-bottom-left-radius";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-bottom-right-radius">MDN
+   *      Web Docs</a>
+   */
+  String borderBottomRightRadius = "border-bottom-right-radius";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-bottom-style">MDN Web
+   *      Docs</a>
+   */
+  String borderBottomStyle = "border-bottom-style";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-bottom-width">MDN Web
+   *      Docs</a>
+   */
+  String borderBottomWidth = "border-bottom-width";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-color">MDN Web Docs</a>
+   */
+  String borderColor = "border-color";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-image">MDN Web Docs</a>
+   */
+  String borderImage = "border-image";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-image-outset">MDN Web
+   *      Docs</a>
+   */
+  String borderImageOutset = "border-image-outset";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-image-repeat">MDN Web
+   *      Docs</a>
+   */
+  String borderImageRepeat = "border-image-repeat";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-image-slice">MDN Web
+   *      Docs</a>
+   */
+  String borderImageSlice = "border-image-slice";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-image-source">MDN Web
+   *      Docs</a>
+   */
+  String borderImageSource = "border-image-source";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-image-width">MDN Web
+   *      Docs</a>
+   */
+  String borderImageWidth = "border-image-width";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-left">MDN Web Docs</a>
+   */
+  String borderLeft = "border-left";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-left-color">MDN Web
+   *      Docs</a>
+   */
+  String borderLeftColor = "border-left-color";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-left-style">MDN Web
+   *      Docs</a>
+   */
+  String borderLeftStyle = "border-left-style";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-left-width">MDN Web
+   *      Docs</a>
+   */
+  String borderLeftWidth = "border-left-width";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-radius">MDN Web Docs</a>
+   */
+  String borderRadius = "border-radius";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-right">MDN Web Docs</a>
+   */
+  String borderRight = "border-right";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-right-color">MDN Web
+   *      Docs</a>
+   */
+  String borderRightColor = "border-right-color";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-right-style">MDN Web
+   *      Docs</a>
+   */
+  String borderRightStyle = "border-right-style";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-right-width">MDN Web
+   *      Docs</a>
+   */
+  String borderRightWidth = "border-right-width";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-style">MDN Web Docs</a>
+   */
+  String borderStyle = "border-style";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-top">MDN Web Docs</a>
+   */
+  String borderTop = "border-top";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-top-color">MDN Web
+   *      Docs</a>
+   */
+  String borderTopColor = "border-top-color";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-top-left-radius">MDN Web
+   *      Docs</a>
+   */
+  String borderTopLeftRadius = "border-top-left-radius";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-top-right-radius">MDN Web
+   *      Docs</a>
+   */
+  String borderTopRightRadius = "border-top-right-radius";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-top-style">MDN Web
+   *      Docs</a>
+   */
+  String borderTopStyle = "border-top-style";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-top-width">MDN Web
+   *      Docs</a>
+   */
+  String borderTopWidth = "border-top-width";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border-width">MDN Web Docs</a>
+   */
+  String borderWidth = "border-width";
+
+
+  /*
+   * Color Properties
+   */
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/color">MDN Web Docs</a>
+   */
+  String color = "color";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/opacity">MDN Web Docs</a>
+   */
+  String opacity = "opacity";
+
+
+  /*
+   * Dimension Properties
+   */
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/height">MDN Web Docs</a>
+   */
+  String height = "height";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/max-height">MDN Web Docs</a>
+   */
+  String maxHeight = "max-height";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/max-width">MDN Web Docs</a>
+   */
+  String maxWidth = "max-width";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/min-height">MDN Web Docs</a>
+   */
+  String minHeight = "min-height";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/min-width">MDN Web Docs</a>
+   */
+  String minWidth = "min-width";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/width">MDN Web Docs</a>
+   */
+  String width = "width";
+
+
+  /*
+   * Font Properties
+   */
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/font">MDN Web Docs</a>
+   */
+  String font = "font";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/font-family">MDN Web Docs</a>
+   */
+  String fontFamily = "font-family";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/font-size">MDN Web Docs</a>
+   */
+  String fontSize = "font-size";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/font-size-adjust">MDN Web
+   *      Docs</a>
+   */
+  String fontSizeAdjust = "font-size-adjust";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/font-stretch">MDN Web Docs</a>
+   */
+  String fontStretch = "font-stretch";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/font-style">MDN Web Docs</a>
+   */
+  String fontStyle = "font-style";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/font-variant">MDN Web Docs</a>
+   */
+  String fontVariant = "font-variant";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight">MDN Web Docs</a>
+   */
+  String fontWeight = "font-weight";
+
+
+  /*
+   * Margin Properties
+   */
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/margin">MDN Web Docs</a>
+   */
+  String margin = "margin";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/margin-bottom">MDN Web Docs</a>
+   */
+  String marginBottom = "margin-bottom";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/margin-left">MDN Web Docs</a>
+   */
+  String marginLeft = "margin-left";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/margin-right">MDN Web Docs</a>
+   */
+  String marginRight = "margin-right";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/margin-top">MDN Web Docs</a>
+   */
+  String marginTop = "margin-top";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/margin-inline-start">MDN Web
+   *      Docs</a>
+   */
+  String marginInlineStart = "margin-inline-start";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/margin-inline-end">MDN Web
+   *      Docs</a>
+   */
+  String marginInlineEnd = "margin-inline-end";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/margin-block-start">MDN Web
+   *      Docs</a>
+   */
+  String marginBlockStart = "margin-block-start";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/margin-block-end">MDN Web
+   *      Docs</a>
+   */
+  String marginBlockEnd = "margin-block-end";
+
+
+  /*
+   * Outline Properties
+   */
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/outline">MDN Web Docs</a>
+   */
+  String outline = "outline";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/outline-color">MDN Web Docs</a>
+   */
+  String outlineColor = "outline-color";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/outline-offset">MDN Web Docs</a>
+   */
+  String outlineOffset = "outline-offset";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/outline-style">MDN Web Docs</a>
+   */
+  String outlineStyle = "outline-style";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/outline-width">MDN Web Docs</a>
+   */
+  String outlineWidth = "outline-width";
+
+
+  /*
+   * Padding Properties
+   */
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/padding">MDN Web Docs</a>
+   */
+  String padding = "padding";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/padding-bottom">MDN Web Docs</a>
+   */
+  String paddingBottom = "padding-bottom";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/padding-left">MDN Web Docs</a>
+   */
+  String paddingLeft = "padding-left";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/padding-right">MDN Web Docs</a>
+   */
+  String paddingRight = "padding-right";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/padding-top">MDN Web Docs</a>
+   */
+  String paddingTop = "padding-top";
+
+
+  /*
+   * Text Properties
+   */
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/direction">MDN Web Docs</a>
+   */
+  String direction = "direction";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/tab-size">MDN Web Docs</a>
+   */
+  String tabSize = "tab-size";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/text-align">MDN Web Docs</a>
+   */
+  String textAlign = "text-align";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/text-align-last">MDN Web
+   *      Docs</a>
+   */
+  String textAlignLast = "text-align-last";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/text-decoration">MDN Web
+   *      Docs</a>
+   */
+  String textDecoration = "text-decoration";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/text-decoration-color">MDN Web
+   *      Docs</a>
+   */
+  String textDecorationColor = "text-decoration-color";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/text-decoration-line">MDN Web
+   *      Docs</a>
+   */
+  String textDecorationLine = "text-decoration-line";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/text-decoration-style">MDN Web
+   *      Docs</a>
+   */
+  String textDecorationStyle = "text-decoration-style";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/text-indent">MDN Web Docs</a>
+   */
+  String textIndent = "text-indent";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/text-justify">MDN Web Docs</a>
+   */
+  String textJustify = "text-justify";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/text-overflow">MDN Web Docs</a>
+   */
+  String textOverflow = "text-overflow";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/text-shadow">MDN Web Docs</a>
+   */
+  String textShadow = "text-shadow";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/text-transform">MDN Web Docs</a>
+   */
+  String textTransform = "text-transform";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/line-height">MDN Web Docs</a>
+   */
+  String lineHeight = "line-height";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/vertical-align">MDN Web Docs</a>
+   */
+  String verticalAlign = "vertical-align";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/letter-spacing">MDN Web Docs</a>
+   */
+  String letterSpacing = "letter-spacing";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/word-spacing">MDN Web Docs</a>
+   */
+  String wordSpacing = "word-spacing";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/white-space">MDN Web Docs</a>
+   */
+  String whiteSpace = "white-space";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/word-break">MDN Web Docs</a>
+   */
+  String wordBreak = "word-break";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/word-wrap">MDN Web Docs</a>
+   */
+  String wordWrap = "word-wrap";
+
+
+  /*
+   * Visual formatting Properties
+   */
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/display">MDN Web Docs</a>
+   */
+  String display = "display";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/position">MDN Web Docs</a>
+   */
+  String position = "position";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/top">MDN Web Docs</a>
+   */
+  String top = "top";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/right">MDN Web Docs</a>
+   */
+  String right = "right";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/bottom">MDN Web Docs</a>
+   */
+  String bottom = "bottom";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/left">MDN Web Docs</a>
+   */
+  String left = "left";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/float">MDN Web Docs</a>
+   */
+  String float_ = "float";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/clear">MDN Web Docs</a>
+   */
+  String clear = "clear";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/z-index">MDN Web Docs</a>
+   */
+  String zIndex = "z-index";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/overflow">MDN Web Docs</a>
+   */
+  String overflow = "overflow";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/overflow-x">MDN Web Docs</a>
+   */
+  String overflowX = "overflow-x";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/overflow-y">MDN Web Docs</a>
+   */
+  String overflowY = "overflow-y";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/resize">MDN Web Docs</a>
+   */
+  String resize = "resize";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/clip">MDN Web Docs</a>
+   */
+  String clip = "clip";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/visibility">MDN Web Docs</a>
+   */
+  String visibility = "visibility";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/cursor">MDN Web Docs</a>
+   */
+  String cursor = "cursor";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow">MDN Web Docs</a>
+   */
+  String boxShadow = "box-shadow";
+
+  /**
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/box-sizing">MDN Web Docs</a>
+   */
+  String boxSizing = "box-sizing";
+
+}
+
