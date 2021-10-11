cf0eb48e8460aa41d550f7db7858ba17af701bf2
Patrick Schmidt <patrick@codecamp.de>
2021-10-11 14:30:17 +0200

    Fix release script.

--- preprel.sh
+++ preprel.sh
@@ -7,6 +7,7 @@ fi
 RELEASE_VERSION=$1
 RELEASE_TAG="release-${RELEASE_VERSION}"
 
+echo ">>>>>>>>>>>>>>> Release version: ${RELEASE_VERSION} <<<<<<<<<<<<<<<"
 ./mvnw versions:set -DnewVersion=${RELEASE_VERSION}
 sed -i "s:<version>.*</version>:<version>${RELEASE_VERSION}</version>:g" README.md
 
@@ -16,7 +17,8 @@ git tag -a -m "RELEASE: ${RELEASE_VERSION}" ${RELEASE_TAG}
 
 
 ./mvnw versions:set -DnextSnapshot=true
-NEXT_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
+NEXT_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout | tail -n 1)
+echo ">>>>>>>>>>>>>>> Next snapshot version: ${NEXT_VERSION} <<<<<<<<<<<<<<<"
 sed -i "s:<version>.*</version>:<version>${NEXT_VERSION}</version>:g" README.md
 
 git add -u
