218db179c80ebb8351adbcf7382afa0df3091fe1
Patrick Schmidt <patrick@codecamp.de>
2021-10-21 07:52:13 +0200

    Make TemplateId and FragmentId inherited.

--- src/main/java/de/codecamp/vaadin/flowdui/FragmentId.java
+++ src/main/java/de/codecamp/vaadin/flowdui/FragmentId.java
@@ -2,17 +2,19 @@ package de.codecamp.vaadin.flowdui;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 
 /**
- * Used to annotate subclasses of {@link FragmentComposite} in order to override the default
- * fragment ID. The default fragment ID is the simple name of the class.
+ * Used to annotate fragment hosts in order to override the default fragment ID. The default
+ * fragment ID is the simple name of the class.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 @Documented
 public @interface FragmentId
 {
--- src/main/java/de/codecamp/vaadin/flowdui/TemplateId.java
+++ src/main/java/de/codecamp/vaadin/flowdui/TemplateId.java
@@ -2,17 +2,19 @@ package de.codecamp.vaadin.flowdui;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 
 /**
- * Used to annotate subclasses of {@link TemplateComposite} in order to override the default
- * template ID. The default template ID is the fully qualified name of the class.
+ * Used to annotate template hosts in order to override the default template ID. The default
+ * template ID is the fully qualified name of the class.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 @Documented
 public @interface TemplateId
 {
